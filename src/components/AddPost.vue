7df39c2c4d3f38adbf6bc889fc6194de4d86b863
brankaDakic10 <branka.dakic@yahoo.com>
2018-05-11 19:30:53 +0200

    Added 5.Task

--- src/components/AddPost.vue
+++ src/components/AddPost.vue
@@ -4,16 +4,18 @@
     <h4>Add new post</h4>
      <form @submit.prevent="submit">
             <div class="form-group">
-                <label for="title">Title</label>
+                <label for="title">Title:</label>
                 <input type="text" id="title" v-model="newPost.title" class="form-control"  placeholder=" Enter blog title " >
             </div>
             <div class="form-group">
-                <label for="text">Text</label>
+                <label for="text">Text:</label>
                 <input type="text" id="text" v-model="newPost.text" class="form-control"  placeholder=" Enter blog text " >
             </div>
             <div class="form-group">
             <button class="btn btn-success">Submit</button> 
+            <button class="btn btn-info" type="reset">Reset</button> 
             </div>
+            
      </form>
     </article>
   </div>
