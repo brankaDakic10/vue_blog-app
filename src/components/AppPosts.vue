8dce1fdf3f698274c017e104e14c93034390c72c
brankaDakic10 <branka.dakic@yahoo.com>
2018-05-11 18:39:10 +0200

    Added 2.Task

--- src/components/AppPosts.vue
+++ src/components/AppPosts.vue
@@ -1,11 +1,48 @@
 <template>
-  <div class="container">
-    All Posts
+ 
+     <div class="container">
+      
+
+<div class="card" style="width: 18rem;" v-for="(post, key) in posts"
+      :key="key">
+  
+  <div class="card-body">
+    <h5 class="card-title">{{post.title}}</h5>
+    
+    <p class="card-text">{{post.text}}</p>
+   
+   
   </div>
+</div>
+
+
+
+  </div>
+
+
+
+ 
 </template>
 <script>
+import {posts} from '../services/Posts'
+
 export default {
+    data(){
+      return{
+         posts:[]
+      }
+    },
+     created() {
+      posts.getAll()
+          .then((response)=> {
+            this.posts=response.data
+             console.log(this.posts)
+         }).catch((error)=> {
+            console.log(error)
+         })  
   
+   
+  }
 }
 </script>
 <style>
